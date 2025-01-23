e4e94419ffe9d49088448c49399fa1ff07a41f3e
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2025-01-23 19:01:06 +0000

    add base and menu template

--- menu/views.py
+++ menu/views.py
@@ -4,5 +4,6 @@ from .models import MenuItem
 
 class MenuList(generic.ListView):
     queryset = MenuItem.objects.all()
-    template_name = "menuitem_list.html"
+    #template_name = "menuitem_list.html"
+    template_name = "menu/menu.html"
 
