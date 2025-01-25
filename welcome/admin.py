4162507c47f924540efba4c7580eece4bf773aab
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2025-01-25 16:24:39 +0000

    refactor welcome admin: promotions

--- welcome/admin.py
+++ welcome/admin.py
@@ -5,8 +5,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(WelcomeInfo)
 class AboutAdmin(SummernoteModelAdmin):
 
-    list_display = ('welcome_title', 'description','contact_info')
-    search_fields = ['welcome_title','description']
+    list_display = ('promotion_title', 'is_valid')
+    search_fields = ['promotion_title','description']
 
 # Register your models here.
 #admin.site.register(WelcomeInfo)
--- welcome/models.py
+++ welcome/models.py
@@ -9,11 +9,11 @@ VALID_CHOICES = [
 
 # Create your models here.
 class WelcomeInfo(models.Model):
-    welcome_title = models.CharField(max_length=150)    
-    description = models.TextField()
-    contact_info = models.TextField()
+    promotion_title = models.CharField(max_length=150, default="null")    
+    description = models.TextField(blank = True)
+    #contact_info = models.TextField()
     is_valid = models.CharField(max_length=15, choices=VALID_CHOICES, blank=False)    
     added_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Main Heading: {self.welcome_title} Current: {self.is_valid} Active since: {self.added_on}"
+        return f"Main Heading: {self.promotion_title}"
