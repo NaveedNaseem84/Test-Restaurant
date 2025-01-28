85e954cab69f378b4cac1efca2e5adc1ebfadcdb
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2025-01-28 14:01:32 +0000

    add booking model and register for admin

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
+from .models import MakeBooking
 # Register your models here.
+
+@admin.register(MakeBooking)
+class MenuAdmin(admin.ModelAdmin):
+
+    list_display = ('user','name', 'email','phone', 'date', 'time_slot', 'number_of_people')
+    search_fields = ['name','email']
+    list_filter = ('name','email')
+   
--- book/models.py
+++ book/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+TIME_SLOTS = [
+    ("5.30","5.30"),
+    ("6.30","6.30"),
+    ("7.30","7.30"),
+    ("8.30","8.30"),
+    ("9.30","9.30"),
+]
+
+NO_OF_PEOPLE = [
+    (1,1),
+    (2,2),
+    (3,3),
+    (4,4),
+    (5,5),
+    (6,6),
+]
 # Create your models here.
+
+class MakeBooking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    date = models.DateField()
+    time_slot = models.CharField(choices=TIME_SLOTS)
+    number_of_people = models.IntegerField(choices=NO_OF_PEOPLE)
+
+    def __str__(self):
+        return f"Booking for: {self.name} on {self.date} at {self.time_slot} for {self.number_of_people}"
