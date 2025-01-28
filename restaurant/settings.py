36f6fcf6434e8b6e00dd7cd3b1ea8d5dcdff3083
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2025-01-28 17:10:32 +0000

    post booking into db working!

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-naveednasee-testrestaur-vduupwiw8hi.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
-LOGIN_REDIRECT_URL = '/book/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
