2d4c559fbc8d37e77ad669729486cfad0e101b39
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2025-01-28 18:38:15 +0000

    show bookings per user

--- book/urls.py
+++ book/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.page_view, name='page_view'),
+    path('book/', views.show_bookings, name='show_bookings'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
-from .models import models
+from .models import MakeBooking
 from .forms import BookingForm
 
+
 def page_view(request):
+
 
     if request.method == "POST":
         form = BookingForm(data=request.POST)
         if form.is_valid():
             form.save()          
     form = BookingForm()
+    bookings = MakeBooking.objects.all() 
 
     return render (
         request, 'book/book.html',
         {
-            "form": form
+            "form": form,
+            "bookings": bookings
         }
         )
-    
+def show_bookings(request):
+    bookings = MakeBooking.objects.all()    
+    return render (
+        request, 'bookings.html',
+        {
+            "bookings": bookings
+        }
+    )
 
