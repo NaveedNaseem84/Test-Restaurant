699a42f2df644748a9511dc39c82f85f1a0b8308
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2025-01-30 12:53:23 +0000

    add in comments and admin on user selection

--- book/views.py
+++ book/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import MakeBooking
 from .forms import BookingForm
 
@@ -9,23 +10,29 @@ def page_view(request):
     if request.method == "POST":
         form = BookingForm(data=request.POST)
         if form.is_valid():
-            form.save()          
+            form.save()
+            messages.add_message(
+        request, messages.SUCCESS,
+        'Booking created.'
+    )          
     form = BookingForm(initial={'name':request.user})
     
-    bookings = MakeBooking.objects.all() 
+    bookings = MakeBooking.objects.all()
+    booking_count = bookings.filter(name = request.user).count()
     #bookings = MakeBooking.objects.filter(user=request.user) 
 
     return render (
         request, 'book/book.html',
         {
             "form": form,
-            "bookings": bookings
+            "bookings": bookings,
+            "booking_count": booking_count
         }
         )
 def show_bookings(request):
     bookings = MakeBooking.objects.all()    
     return render (
-        request, 'bookings.html',
+        request, 'book/bookings.html',
         {
             "bookings": bookings
         }
