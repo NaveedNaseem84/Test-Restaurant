362d8f62b026be37dfe92b603777b0568b23ffc0
NaveedNaseem84 <naveed.naseem@nwas.nhs.uk>
2025-02-04 17:00:18 +0000

    add modal to delete

--- static/js/bookings.js
+++ static/js/bookings.js
@@ -1,11 +1,20 @@
 const deleteButtons = document.getElementsByClassName("btn-danger");
 const editButtons = document.getElementsByClassName("btn-secondary-update");
 
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteConfirm = document.getElementById("deleteConfirm");
+
 for (let button of deleteButtons) {
-    button.addEventListener("click", (e) => {
-        let bookingId = e.target.getAttribute("data-booking-id");
-        window.location.href=`delete_booking/${bookingId}`;        
+  button.addEventListener("click", (e) => {
+    let bookingId = e.target.getAttribute("data-booking-id");
+    deleteModal.show();
+
+    deleteConfirm.addEventListener("click", () => {
+      if (bookingId) {
+        window.location.href = `delete_booking/${bookingId}`;
+      }
     });
+  });
 }
 
 for (let button of editButtons) {
